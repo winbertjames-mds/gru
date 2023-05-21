7292b4cf522af1832a575066191abfebd28d71c2
Winbert James <134004681+winbertjames-mds@users.noreply.github.com>
2023-05-21 10:30:11 +0800

    Update final.py

--- final.py
+++ final.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 
 # Load the GRU model
 model = load_model('gru_model.hdf5')
@@ -16,7 +17,7 @@ submission_data['Date'] = pd.to_datetime(submission_data['Date'], format='%d-%b-
 st.title('Student Submission Predictor')
 
 # Input field for date
-input_date = st.text_input('Enter a specific date in %d-%b-%Y format', '01-Jan-2023')
+input_date = st.text_input('Enter a specific date in DD-MMM-YYYY format', '01-Jan-2023')
 
 # Parse the input date
 try:
@@ -32,9 +33,18 @@ filtered_data = submission_data[submission_data['Date'] == input_date_str]
 if len(filtered_data) > 0:
     # Get the features for the input date
     input_features = filtered_data.drop(['Date', 'Submission'], axis=1).values
+    
+    # Reshape the input features for compatibility with the model
+    input_features = input_features.reshape((input_features.shape[0], input_features.shape[1], 1))
+
+    # Create a dummy target variable for TimeseriesGenerator
+    target_dummy = [0] * len(input_features)
+
+    # Generate sequences using TimeseriesGenerator
+    data_generator = TimeseriesGenerator(input_features, target_dummy, length=5, batch_size=1)
 
     # Predict the submission number
-    prediction = model.predict(input_features)[0][0]
+    prediction = model.predict(data_generator)[0][0]
 
     # Display the prediction
     st.success(f'Predicted submission number for {input_date_str}: {prediction:.2f}')
